c56cdb0d3668a7fb8c338aabadc30c78c6737f07
daidong <daidong@node-2.daidong-qv18171.cloudincr-pg0.utah.cloudlab.us>
2016-08-23 17:09:07 -0600

    update fish as default shell

--- installation/install.sh
+++ installation/install.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get update -y
 sudo apt-get install -y wget curl axel ruby python python3 fish build-essential cmake gcc python-setuptools software-properties-common xz-utils 
-curl -L http://get.oh-my.fish | fish
 echo "fish" >> ~/.profile
-omf install cbjohnson
+curl -L http://get.oh-my.fish > installomf
+fish installomf --noninteractive
+rm -rf installomf
