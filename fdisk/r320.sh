07cdfebbdc241bbd6c1074007ffa76d885e2b9e5
daidong <daidong@node-0.daidong-qv18168.cloudincr-pg0.wisc.cloudlab.us>
2016-08-26 04:17:33 -0600

    change r320.sh

--- fdisk/r320.sh
+++ fdisk/r320.sh
@@ -19,15 +19,15 @@ ls /dev/ |  grep sd
 read TGTDEV
 fi
 if [ -n "${TGTDEV}" ]; then
-	sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
-	  o # clear the in memory partition table
-	  n # new partition
-	  p # primary partition
-	  1 # partition number 1
-	    # default - start at beginning of disk 
-	    # default, extend partition to end of disk
-	  p # print the in-memory partition table
-	  w # write the partition table
-	  q # and we're done
-	EOF
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+    # default, extend partition to end of disk
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
 fi
