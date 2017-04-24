d20eaf876ed08b99f2376af5b3ee4df877e4113e
Zhang Wei <zhangwei217245@gmail.com>
2017-04-23 20:20:12 -0500

    add docker capability as basic software
    add docker capability as basic software

--- installation/install.sh
+++ installation/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y wget curl axel ruby python python3 fish build-essential cmake gcc python-setuptools software-properties-common xz-utils 
+sudo apt-get install -y wget curl axel ruby python python3 fish build-essential cmake gcc python-setuptools software-properties-common xz-utils docker.io docker-registry docker-compose docker-doc docker
 echo "fish" >> ~/.profile
 curl -L http://get.oh-my.fish > installomf
 fish installomf --noninteractive
