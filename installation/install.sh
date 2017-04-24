fe7e982200a6c95108e9a93d73e3ebd85ef7a999
Zhang Wei <zhangwei217245@gmail.com>
2017-04-23 22:37:49 -0500

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt upgrade -y
 sudo apt-get install -y wget curl axel ruby python python3 fish build-essential cmake gcc python-setuptools software-properties-common xz-utils docker.io docker-registry docker-compose docker-doc docker
 echo "fish" >> ~/.profile
 curl -L http://get.oh-my.fish > installomf
