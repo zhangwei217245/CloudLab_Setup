e7c72c1f489fa5d385f39571f8d3b0143d14ce8a
zhang56 <zhang56@node-0.zhang56-qv25099.cloudincr-pg0.utah.cloudlab.us>
2017-04-26 14:14:57 -0600

    commit install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -2,6 +2,13 @@
 
 sudo apt-get update -y
 sudo apt-get install -y wget curl axel ruby python python3 fish build-essential cmake gcc python-setuptools software-properties-common xz-utils docker.io docker-registry docker-compose docker-doc docker
+
+service docker stop
+sleep 3s
+mkdir -p /data/software/docker ; mv /var/lib/docker/* /data/software/; rm -rf /var/lib/docker
+ln -s /data/software/docker /var/lib/docker
+service docker start
+
 echo "fish" >> ~/.profile
 curl -L http://get.oh-my.fish > installomf
 fish installomf --noninteractive
