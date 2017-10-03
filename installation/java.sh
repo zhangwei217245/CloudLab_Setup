3cbb2e869b96ad5998d9acaf62920b05b3789934
zhang56 <zhang56@node-0.zhang56-qv29097.cloudincr-pg0.utah.cloudlab.us>
2017-10-03 12:20:34 -0600

    fix java

--- installation/java.sh
+++ installation/java.sh
@@ -2,8 +2,8 @@
 
 sudo add-apt-repository -y ppa:webupd8team/java 
 sudo apt-get update -y
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer ant maven gradle
+echo "oracle-java9-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java9-installer ant maven gradle
 java -version
 javac -version
 ant -v
