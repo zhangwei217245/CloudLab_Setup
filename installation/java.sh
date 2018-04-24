56b61a9c6e476cf9e03b990d77f74bcee08f5269
Zhang Wei <zhangwei217245@gmail.com>
2018-04-23 23:51:21 -0500

    Update java.sh

--- installation/java.sh
+++ installation/java.sh
@@ -4,9 +4,9 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo add-apt-repository -y ppa:linuxuprising/java
 sudo apt-get update -y
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-echo "oracle-java9-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-echo "oracle-java10-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java10-installer oracle-java9-installer oracle-java8-installer ant maven gradle
+#echo "oracle-java9-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+#echo "oracle-java10-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer ant maven gradle
 java -version
 javac -version
 ant -v
