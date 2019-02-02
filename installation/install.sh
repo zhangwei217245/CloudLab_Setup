274b3326b7d661aa57b3604f68db3df6bf19abad
Zhang Wei <zhangwei217245@gmail.com>
2019-02-02 15:02:53 -0500

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -2,14 +2,8 @@
 
 sudo apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade 
-sudo apt install -y wget curl axel ruby python python3 fish build-essential cmake gcc python-setuptools software-properties-common xz-utils docker.io docker-compose docker-doc docker
+sudo apt install -y wget curl axel ruby python python3 fish build-essential cmake gcc python-setuptools software-properties-common xz-utils 
 
-service docker stop
-sleep 3s
-mkdir -p /data/software/docker ; mv /var/lib/docker/* /data/software/; rm -rf /var/lib/docker
-ln -s /data/software/docker /var/lib/docker
-service docker start
-sudo usermod -a -G docker zhang56
 
 echo "fish" >> ~/.profile
 curl -L http://get.oh-my.fish > installomf
