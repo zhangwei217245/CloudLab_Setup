bca1b9c9e69d732bbf459933aa2862db07c43223
zhang56 <zhang56@ms1032.utah.cloudlab.us>
2019-02-23 20:33:03 -0700

    change a script

--- secure/secure.sh
+++ secure/secure.sh
@@ -2,5 +2,5 @@
 
 CURRENT_USER=`users`
 
-ls -l --color=never /users/ | grep -v "total"|egrep -v "(${CURRENT_USER}|geniuser)"|awk '{print "/users/"$NF}'|xargs rm -rf
+ls -l --color=never /users/ | grep -v "total"|egrep -v "(${CURRENT_USER}|geniuser)"|awk '{print "/users/"$NF}'|sudo xargs rm -rf
 
